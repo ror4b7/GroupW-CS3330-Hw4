f5f12150099741246ca478c351734fda96f81c94
RougeAce <Trey722@users.noreply.github.com>
2024-02-19 21:54:56 -0600

    Updated for justice file strcucture

--- Homework4/src/hw4/vehicle/Motorbike.java
+++ Homework4/src/hw4/vehicle/Motorbike.java
@@ -1,5 +1,28 @@
 package hw4.vehicle;
 
-public class Motorbike {
+public class Motorbike extends Vehicle {
+
+    public MotorBike(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType, double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+        super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+    }
+    //Setting up my classes : Trey
+    @Override
+    public abstract double calculateMaintenaceCost(double distance)
+    {
+    }
+
+     //Setting up my classes : Trey
+    @Override
+    public abstract double calculateFuelEfficiency(double distance, double fuelPrice)
+    {
+    }
+
+     //Setting up my classes : Trey
+    @Override
+    public abstract void startEngine()
+    {
+        
+    }
+
 
 }
--- Homework4/src/hw4/vehicle/SUV.java
+++ Homework4/src/hw4/vehicle/SUV.java
@@ -1,5 +1,30 @@
 package hw4.vehicle;
 
-public class SUV {
+public class SUV  extends Vehicle
 
+{
+
+    public SUV(String brand, String make, long modelYear, double price, VehicleColor color, FuelType fuelType, double mileage, double mass, int cylinders, double gasTankCapacity, StartMechanism startType) {
+        super(brand, make, modelYear, price, color, fuelType, mileage, mass, cylinders, gasTankCapacity, startType);
+    }
+    //Setting up my classes : Trey
+    @Override
+    public abstract double calculateMaintenaceCost(double distance)
+    {
+    }
+
+    //Setting up my classes : Trey
+    @Override
+    public abstract double calculateFuelEfficiency(double distance, double fuelPrice)
+    {
+    }
+    //Setting up my classes : Trey
+    @Override
+    public abstract void startEngine()
+    {
+        
+    }
+
+
+    
 }

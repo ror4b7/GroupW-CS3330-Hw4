4a7e44d9e033f65ffd086640636d8756628edab0
RougeAce <Trey722@users.noreply.github.com>
2024-02-19 22:00:48 -0600

    Added VechicalManager Class

--- Homework4/src/hw4/vehicleManager/VehicleManager.java
+++ Homework4/src/hw4/vehicleManager/VehicleManager.java
@@ -1,5 +1,13 @@
 package hw4.vehicleManager;
+import java.util.ArrayList;
+import hw4.vehicle.*;;
 
 public class VehicleManager {
+
+    private ArrayList<Vehicle> vehicleList; 
+    private String vehicleFilePath; 
+    
+
+
 
 }

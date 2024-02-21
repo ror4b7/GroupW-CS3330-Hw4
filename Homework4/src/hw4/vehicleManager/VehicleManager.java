20a46dec8e494da8a975359822c7bcf2f2e8b04b
RougeAce <Trey722@users.noreply.github.com>
2024-02-21 12:43:04 -0600

    Added  getAverageFuelEfficiencyOfSUVs(double distance, double fuelPrice)

--- Homework4/src/hw4/vehicleManager/VehicleManager.java
+++ Homework4/src/hw4/vehicleManager/VehicleManager.java
@@ -9,6 +9,15 @@ public class VehicleManager {
 
 
 
+    //Sorry Kailtyn I needed to type something so I can make the fueEfficency function - Trey 
+    private boolean isVehicleType(Vehicle v, Class clazz)
+    {
+
+            return true; 
+    }
+
+
+
     //removes the first instance of the vehcial provided
     //Returns false if none are found
     public boolean removeVehicle(Vehicle vehicle)
@@ -55,9 +64,43 @@ public class VehicleManager {
 
     }
 
+    
+
+
+
+    //Gets the average of fuel efficencty by averaging gas tank capcity and cylinder and then running the equation
     public double getAverageFuelEfficiencyOfSUVs(double distance, double fuelPrice)
     {
 
+        //Find average cylinder
+        //find average gas tank capcity 
+        double cylinder = 0;
+        double gasTankCapacity = 0; 
+        int count = 0; 
+
+        for (Vehicle cur : vehicleList)
+        {
+            if (isVehicleType(cur, cur.getClass()))
+            {
+
+                cylinder += cur.getCylinder(); 
+                gasTankCapacity += cur.getGasTankCapcity; 
+                count += 1; 
+            }
+        }
+
+
+        if (count == 0)
+        {
+            return -1.0; 
+        }
+
+        else 
+        {
+            return (cylinder / count) * (gasTankCapacity / count) * fuelPrice / distance * 0.05;
+        }
+        
+        
     }
 
     
